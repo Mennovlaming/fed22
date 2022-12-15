4b85e2d9666b16efef83a3843358035d5ba7860e
Menno Vlaming <mennovlaming@gmail.com>
2022-12-15 14:03:42 +0100

    Js cleanup, html semantisch, css aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,26 @@
-const hamburger = document.querySelector("header nav:first-of-type button img");
-const overlayNav = document.querySelector("header nav:nth-of-type(2)");
+const hamburgerButton = document.querySelector("header nav:nth-of-type(1) button");
+const hamburgerMenu = document.querySelector("header nav:nth-of-type(2)");
+const plusButtons = document.querySelectorAll("header nav:nth-of-type(2) > ul > li button");
 
-const onzeMerken = document.querySelector(".openList");
-const plusButton = document.querySelector(".merkenList button");
-const plusImage = document.querySelector(".merkenList button img")
 
-//  const heightUl = document.querySelector(".overlay-content");
-//  let height = heightUl.offsetHeight;
-
-let toggleList = true;
-let toggleHamburger = true;
-
-const openMenu = () => {
-    overlayNav.classList.toggle('heightOverlay');
-    if (toggleHamburger === true) {
-        hamburger.src = "/images/icons/close.png"
-    }else {
-        hamburger.src = "/images/icons/hamburger.png";
-    }
-    toggleHamburger = !toggleHamburger;  
+const toggleHamburgerMenu = () => {
+    hamburgerMenu.classList.toggle('open');
+    hamburgerButton.classList.toggle('open'); 
 }
 
-// if plusbutton is clicked, trigger changeimg
-const changeImg = () => {
-    if (toggleList === true) {
-        plusImage.src = "/images/icons/minus.png";
-    } else {
-        plusImage.src = "/images/icons/plus2.png";
-    }
-    toggleList = !toggleList;
+// anders werkt this niet :-(
+const toggleSubMenu = () => {
+    const dePlusButton = this;
+    const hetSubMenu =  dePlusButton.nextElementSibling;
+
+    dePlusButton.classList.toggle('open');
+    hetSubMenu.classList.toggle('open');
 }
 
-const openLi = () => {
-    onzeMerken.classList.toggle('openLi');
-}
- 
-const openLiChangeImg = () => {
-    changeImg();
-    openLi();
-}
 
-hamburger.addEventListener("click", openMenu);
-plusButton.addEventListener("click", openLiChangeImg);
+hamburgerButton.addEventListener("click", toggleHamburgerMenu);
+
+plusButtons.forEach(plusButton => {
+    plusButton.addEventListener("click", toggleSubMenu);
+});
+
